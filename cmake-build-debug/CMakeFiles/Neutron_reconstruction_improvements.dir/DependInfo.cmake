e1cffb4e5435a072cc4587a0219fd4c2da2e75a1
alons126 <102910135+alons126@users.noreply.github.com>
2024-08-10 15:18:48 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Neutron_reconstruction_improvements.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alon/Neutron_reconstruction_improvements/Original/TMVAClassification.C" "/home/alon/Neutron_reconstruction_improvements/cmake-build-debug/CMakeFiles/Neutron_reconstruction_improvements.dir/Original/TMVAClassification.C.o"
+  "/home/alon/Neutron_reconstruction_improvements/Original/TMVAClassificationApplication.C" "/home/alon/Neutron_reconstruction_improvements/cmake-build-debug/CMakeFiles/Neutron_reconstruction_improvements.dir/Original/TMVAClassificationApplication.C.o"
+  "/home/alon/Neutron_reconstruction_improvements/TMVAClassification.C" "/home/alon/Neutron_reconstruction_improvements/cmake-build-debug/CMakeFiles/Neutron_reconstruction_improvements.dir/TMVAClassification.C.o"
+  "/home/alon/Neutron_reconstruction_improvements/TMVAClassificationApplication.C" "/home/alon/Neutron_reconstruction_improvements/cmake-build-debug/CMakeFiles/Neutron_reconstruction_improvements.dir/TMVAClassificationApplication.C.o"
+  "/home/alon/Neutron_reconstruction_improvements/main.cpp" "/home/alon/Neutron_reconstruction_improvements/cmake-build-debug/CMakeFiles/Neutron_reconstruction_improvements.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/mnt/c/root_v6.26.10/include"
+  "/home/alon/clas12root/Clas12Root"
+  "/home/alon/clas12root/Clas12Banks"
+  "/home/alon/clas12root/hipo4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Neutron_reconstruction_improvements.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Neutron_reconstruction_improvements.dir/Original/TMVAClassification.C.o"
+  "CMakeFiles/Neutron_reconstruction_improvements.dir/Original/TMVAClassificationApplication.C.o"
+  "CMakeFiles/Neutron_reconstruction_improvements.dir/TMVAClassification.C.o"
+  "CMakeFiles/Neutron_reconstruction_improvements.dir/TMVAClassificationApplication.C.o"
+  "CMakeFiles/Neutron_reconstruction_improvements.dir/main.cpp.o"
+  "Neutron_reconstruction_improvements"
+  "Neutron_reconstruction_improvements.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Neutron_reconstruction_improvements.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
