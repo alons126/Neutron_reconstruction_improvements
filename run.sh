6fc16e1f9d530888e35bf8173705589a2e64bc55
alons126 <alonsportes@mail.tau.ac.il>
2024-11-21 14:41:57 -0500

    first test of Erin's code

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+clang++ -o program D_getfeatures_Phase1.cpp && ./program 5.98636 0 test.root test.txt /cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo
