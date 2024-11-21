5188757bbab537c16a0cab34bfa62a71bc9ee03e
alons126 <alonsportes@mail.tau.ac.il>
2024-11-21 14:43:27 -0500

    first test of Erin's code

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-
+echo ""
+git pull
+echo ""
+echo "- Re-pulling repository -----------------------------------------------"
+echo ""
+git reset --hard
+git pull
+echo ""
+echo "- Lunching Erin's D_getfeatures code ----------------------------------"
+echo ""
 clang++ -o program D_getfeatures_Phase1.cpp && ./program 5.98636 0 test.root test.txt /cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo
+echo ""
+echo "- Operation finished --------------------------------------------------"
