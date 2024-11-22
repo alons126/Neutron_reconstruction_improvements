d3cce66d16e6f5525545b7b485327c2494014d7c
alons126 <alonsportes@mail.tau.ac.il>
2024-11-22 11:45:29 -0500

    Combining Erin's code with Andrew's code test

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ git pull
 echo ""
 echo "- Lunching Erin's D_getfeatures code ----------------------------------"
 echo ""
-clas12root -l -q 'D_getfeatures_Phase1.cpp(5.98636, true, "test.root", "test.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo")'
+clas12root -l -q 'D_getfeatures_Phase2.cpp(5.98636, true, "test.root", "test.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo")'
 echo ""
 echo "- Operation finished --------------------------------------------------"
