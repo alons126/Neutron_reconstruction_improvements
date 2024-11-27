54758d7d7f335d6d513d2bcf57c15f96d5d32e2e
alons126 <alonsportes@mail.tau.ac.il>
2024-11-27 11:50:45 -0500

    Fitting Erins and Andrews codes

--- run.sh
+++ run.sh
@@ -9,7 +9,6 @@ git pull
 echo ""
 echo "- Lunching Erin's D_getfeatures code ----------------------------------"
 echo ""
-clas12root -l -q 'D_getfeatures_Phase4.cpp(5.98636, true, "Output/Erin_plots.root", "Output/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "Output/Andrew_plots.pdf")'
-# clas12root -l -q 'D_getfeatures_Phase4.cpp(5.98636, true, "Output/Erin_plots.root", "Output/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "Output/Andrew_plots.pdf")'
+source file_run.sh
 echo ""
 echo "- Operation finished --------------------------------------------------"
