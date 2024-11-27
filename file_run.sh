19b1d256a7423b2fd9c592de95c415a2da3af689
alons126 <alonsportes@mail.tau.ac.il>
2024-11-27 13:06:18 -0500

    Fitting Erins and Andrews codes

--- file_run.sh
+++ file_run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-OUTDIR=Output_trtrtrtr
+OUTDIR="Output_trtrtrtr"
+
+# Use double quotes for correct variable expansion inside ROOT command
+clas12root -l -q "D_getfeatures_Phase4.cpp($OUTDIR, 5.98636, true, \"$OUTDIR/Erin_plots.root\", \"$OUTDIR/Erin_plots.txt\", \"/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo\", \"$OUTDIR/Andrew_plots.pdf\")"
+
+
+
 
 # # All D2 @ 6GeV data:
 # clas12root -l -q 'D_getfeatures_Phase4.cpp($OUTDIR, 5.98636, true, "$OUTDIR/Erin_plots.root", "$OUTDIR/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "$OUTDIR/Andrew_plots.pdf")'
@@ -8,5 +14,5 @@ OUTDIR=Output_trtrtrtr
 ## Only run 015045 of D2 @ 6GeV data:
 # clas12root -l -q 'D_getfeatures_Phase4.cpp($OUTDIR, 5.98636, true, "$OUTDIR/Erin_plots.root", "$OUTDIR/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "$OUTDIR/Andrew_plots.pdf")'
 
-# Only one file of run 015045 of D2 @ 6GeV data:
-clas12root -l -q 'D_getfeatures_Phase4.cpp('"$OUTDIR"', 5.98636, true, "'"$OUTDIR"'/Erin_plots.root", "'"$OUTDIR"'/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "'"$OUTDIR"'/Andrew_plots.pdf")'
+# # Only one file of run 015045 of D2 @ 6GeV data:
+# clas12root -l -q 'D_getfeatures_Phase4.cpp('"$OUTDIR"', 5.98636, true, "'"$OUTDIR"'/Erin_plots.root", "'"$OUTDIR"'/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "'"$OUTDIR"'/Andrew_plots.pdf")'
