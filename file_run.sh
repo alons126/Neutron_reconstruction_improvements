51ec533a82071fe74a1a9d4e72f48a749841d6a4
alons126 <alonsportes@mail.tau.ac.il>
2024-11-27 12:59:40 -0500

    Fitting Erins and Andrews codes

--- file_run.sh
+++ file_run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
-OUTDIR=Output_trtrtrtr
-
+unset OUTDIR
+setenv OUTDIR Output_trtrtrtr
 
 # # All D2 @ 6GeV data:
-# clas12root -l -q 'D_getfeatures_Phase4.cpp($OUTDIR, 5.98636, true, "$OUTDIR/Erin_plots.root", "$OUTDIR/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "$OUTDIR/Andrew_plots.pdf")'
+# clas12root -l -q 'D_getfeatures_Phase4.cpp(${OUTDIR}, 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "${OUTDIR}/Andrew_plots.pdf")'
 
 ## Only run 015045 of D2 @ 6GeV data:
-# clas12root -l -q 'D_getfeatures_Phase4.cpp($OUTDIR, 5.98636, true, "$OUTDIR/Erin_plots.root", "$OUTDIR/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "$OUTDIR/Andrew_plots.pdf")'
+# clas12root -l -q 'D_getfeatures_Phase4.cpp(${OUTDIR}, 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "${OUTDIR}/Andrew_plots.pdf")'
 
 # Only one file of run 015045 of D2 @ 6GeV data:
-clas12root -l -q 'D_getfeatures_Phase4.cpp($OUTDIR, 5.98636, true, "$OUTDIR/Erin_plots.root", "$OUTDIR/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "$OUTDIR/Andrew_plots.pdf")'
+clas12root -l -q 'D_getfeatures_Phase4.cpp(${OUTDIR}, 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "${OUTDIR}/Andrew_plots.pdf")'
