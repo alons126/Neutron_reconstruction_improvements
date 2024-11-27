0a75a781beda1231368f358c75538de99e292c5b
alons126 <alonsportes@mail.tau.ac.il>
2024-11-27 13:19:55 -0500

    Fitting Erins and Andrews codes

--- file_run.sh
+++ file_run.sh
@@ -3,16 +3,10 @@
 # OUTDIR="Output_test"
 
 # # All D2 @ 6GeV data:
-# clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_test", "Output_test/Erin_plots.pdf",
-                                            # 5.98636, true, "Output_test/Erin_plots.root", "Output_test/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*",
-                                            # "Output_test/Andrew_plots.pdf")'
+# clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_test", "Output_test/Erin_plots.pdf", 5.98636, true, "Output_test/Erin_plots.root", "Output_test/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "Output_test/Andrew_plots.pdf")'
 
 ## Only run 015045 of D2 @ 6GeV data:
-# clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_test", "Output_test/Erin_plots.pdf", 
-                                            # 5.98636, true, "Output_test/Erin_plots.root", "Output_test/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo",
-                                            # "Output_test/Andrew_plots.pdf")'
+# clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_test", "Output_test/Erin_plots.pdf", 5.98636, true, "Output_test/Erin_plots.root", "Output_test/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "Output_test/Andrew_plots.pdf")'
 
 # # Only one file of run 015045 of D2 @ 6GeV data:
-clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_test", "Output_test/Erin_plots.pdf", 
-                                            5.98636, true, "Output_test/Erin_plots.root", "Output_test/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", 
-                                            "Output_test/Andrew_plots.pdf")'
+clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_test", "Output_test/Erin_plots.pdf", 5.98636, true, "Output_test/Erin_plots.root", "Output_test/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "Output_test/Andrew_plots.pdf")'
