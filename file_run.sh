70018969e3d7a69d436323eabf2c2e24d35742ca
alons126 <alonsportes@mail.tau.ac.il>
2024-11-27 13:11:32 -0500

    Fitting Erins and Andrews codes

--- file_run.sh
+++ file_run.sh
@@ -1,18 +1,12 @@
-#!/bin/csh
+#!/bin/bash
 
-unset OUTDIR
-setenv OUTDIR Output_trtrtrtr
-
-# Use double quotes for correct variable expansion inside ROOT command
-clas12root -l -q 'D_getfeatures_Phase4.cpp(${OUTDIR}, 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "${OUTDIR}/Andrew_plots.pdf")'
-
-
+# OUTDIR="Output_trtrtrtr"
 
 # # All D2 @ 6GeV data:
-# clas12root -l -q 'D_getfeatures_Phase4.cpp($OUTDIR, 5.98636, true, "$OUTDIR/Erin_plots.root", "$OUTDIR/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "$OUTDIR/Andrew_plots.pdf")'
+# clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_trtrtrtr", 5.98636, true, "Output_trtrtrtr/Erin_plots.root", "Output_trtrtrtr/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "Output_trtrtrtr/Andrew_plots.pdf")'
 
 ## Only run 015045 of D2 @ 6GeV data:
-# clas12root -l -q 'D_getfeatures_Phase4.cpp($OUTDIR, 5.98636, true, "$OUTDIR/Erin_plots.root", "$OUTDIR/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "$OUTDIR/Andrew_plots.pdf")'
+# clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_trtrtrtr", 5.98636, true, "Output_trtrtrtr/Erin_plots.root", "Output_trtrtrtr/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "Output_trtrtrtr/Andrew_plots.pdf")'
 
 # # Only one file of run 015045 of D2 @ 6GeV data:
-# clas12root -l -q 'D_getfeatures_Phase4.cpp('"$OUTDIR"', 5.98636, true, "'"$OUTDIR"'/Erin_plots.root", "'"$OUTDIR"'/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "'"$OUTDIR"'/Andrew_plots.pdf")'
+clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_trtrtrtr", 5.98636, true, "Output_trtrtrtr/Erin_plots.root", "Output_trtrtrtr/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "Output_trtrtrtr/Andrew_plots.pdf")'
