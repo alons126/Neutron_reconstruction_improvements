e89f34addaf2e77937a72749a48d672d087d3e9a
alons126 <alonsportes@mail.tau.ac.il>
2024-12-02 09:52:21 -0500

    Adding Andrews variables to Erins code Output_full_data

--- file_run.sh
+++ file_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# OUTDIR="Output_test"
+# OUTDIR="Output_full_data"
 
-# # All D2 @ 6GeV data:
-# clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_test", "Output_test/Erin_plots.pdf", 5.98636, true, "Output_test/Erin_plots.root", "Output_test/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "Output_test/Andrew_plots.pdf")'
+# All D2 @ 6GeV data:
+clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_full_data", "Output_full_data/Erin_plots.pdf", 5.98636, true, "Output_full_data/Erin_plots.root", "Output_full_data/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "Output_full_data/Andrew_plots.pdf")'
 
 ## Only run 015045 of D2 @ 6GeV data:
-# clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_test", "Output_test/Erin_plots.pdf", 5.98636, true, "Output_test/Erin_plots.root", "Output_test/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "Output_test/Andrew_plots.pdf")'
+# clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_full_data", "Output_full_data/Erin_plots.pdf", 5.98636, true, "Output_full_data/Erin_plots.root", "Output_full_data/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "Output_full_data/Andrew_plots.pdf")'
 
 # # Only one file of run 015045 of D2 @ 6GeV data:
-clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_test", "Output_test/Erin_plots.pdf", 5.98636, true, "Output_test/Erin_plots.root", "Output_test/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "Output_test/Andrew_plots.pdf")'
+# clas12root -l -q 'D_getfeatures_Phase4.cpp("Output_full_data", "Output_full_data/Erin_plots.pdf", 5.98636, true, "Output_full_data/Erin_plots.root", "Output_full_data/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "Output_full_data/Andrew_plots.pdf")'
