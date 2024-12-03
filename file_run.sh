85ddf5bff78cc5eae41842090e19bd523b879178
alons126 <alonsportes@mail.tau.ac.il>
2024-12-03 16:57:26 -0500

    Output_full_data_2_pathTest

--- file_run.sh
+++ file_run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# OUTDIR="Output_full_data_2"
+# OUTDIR="Output_full_data_2_pathTest"
 
 # All D2 @ 6GeV data:
-clas12root -l -q 'D_getfeatures_Phase5.cpp("Output_full_data_2", "Output_full_data_2/Erin_plots.pdf", 5.98636, true, "Output_full_data_2/Erin_plots.root", "Output_full_data_2/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "Output_full_data_2/Andrew_plots.pdf")'
+clas12root -l -q 'D_getfeatures_Phase5.cpp("Output_full_data_2_pathTest", "Output_full_data_2_pathTest/Erin_plots.pdf", 5.98636, true, "Output_full_data_2_pathTest/Erin_plots.root", "Output_full_data_2_pathTest/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "Output_full_data_2_pathTest/Andrew_plots.pdf")'
 
 ## Only run 015045 of D2 @ 6GeV data:
-# clas12root -l -q 'D_getfeatures_Phase5.cpp("Output_full_data_2", "Output_full_data_2/Erin_plots.pdf", 5.98636, true, "Output_full_data_2/Erin_plots.root", "Output_full_data_2/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "Output_full_data_2/Andrew_plots.pdf")'
+# clas12root -l -q 'D_getfeatures_Phase5.cpp("Output_full_data_2_pathTest", "Output_full_data_2_pathTest/Erin_plots.pdf", 5.98636, true, "Output_full_data_2_pathTest/Erin_plots.root", "Output_full_data_2_pathTest/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "Output_full_data_2_pathTest/Andrew_plots.pdf")'
 
 # # Only run 015443 of D2 @ 6GeV data:
-# clas12root -l -q 'D_getfeatures_Phase5.cpp("Output_full_data_2", "Output_full_data_2/Erin_plots.pdf", 5.98636, true, "Output_full_data_2/Erin_plots.root", "Output_full_data_2/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015443/*.hipo", "Output_full_data_2/Andrew_plots.pdf")'
+# clas12root -l -q 'D_getfeatures_Phase5.cpp("Output_full_data_2_pathTest", "Output_full_data_2_pathTest/Erin_plots.pdf", 5.98636, true, "Output_full_data_2_pathTest/Erin_plots.root", "Output_full_data_2_pathTest/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015443/*.hipo", "Output_full_data_2_pathTest/Andrew_plots.pdf")'
 
 # # Only one file of run 015045 of D2 @ 6GeV data:
-# clas12root -l -q 'D_getfeatures_Phase5.cpp("Output_full_data_2", "Output_full_data_2/Erin_plots.pdf", 5.98636, true, "Output_full_data_2/Erin_plots.root", "Output_full_data_2/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "Output_full_data_2/Andrew_plots.pdf")'
+# clas12root -l -q 'D_getfeatures_Phase5.cpp("Output_full_data_2_pathTest", "Output_full_data_2_pathTest/Erin_plots.pdf", 5.98636, true, "Output_full_data_2_pathTest/Erin_plots.root", "Output_full_data_2_pathTest/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "Output_full_data_2_pathTest/Andrew_plots.pdf")'
