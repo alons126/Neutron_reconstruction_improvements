705a52689da5537568ce9604dd3be04d1522fdcb
alons126 <alonsportes@mail.tau.ac.il>
2024-12-03 15:27:50 -0500

    new plots test

--- D_getfeatures_Phase5.cpp
+++ D_getfeatures_Phase5.cpp
@@ -2838,11 +2838,12 @@ int D_getfeatures_Phase5(                                                       
     TLatex text_Step0;
     text_Step0.SetTextSize(0.05);
 
-    const char *pdfFile_Step0 = ConfigOutPutName(PDFFile, "Step0").c_str();
+    string pdfFile_Step0_0 = ConfigOutPutName(PDFFile, "Step0").c_str();
+    const char *pdfFile_Step0 = pdfFile_Step0_0.c_str();
 
     cout << "\nPDFFile = " << PDFFile << "\n";
-    cout << "\nConfigOutPutName(PDFFile, 'Step0') = " << ConfigOutPutName(PDFFile, "Step0") << "\n";
-    cout << "\n(ConfigOutPutName(PDFFile, 'Step0')).c_str() = " << (ConfigOutPutName(PDFFile, "Step0")).c_str() << "\n";
+    cout << "\npdfFile_Step0_0 = " << pdfFile_Step0_0 << "\n";
+    cout << "\npdfFile_Step0 = " << pdfFile_Step0 << "\n";
 
     char fileName_Step0[100];
     sprintf(fileName_Step0, "%s[", pdfFile_Step0);
