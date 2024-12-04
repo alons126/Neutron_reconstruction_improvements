97e682178334d80640b93ea36ba2571c14485dfe
alons126 <alonsportes@mail.tau.ac.il>
2024-12-04 10:04:59 -0500

    Adding my own changes

--- file_run.sh
+++ file_run.sh
@@ -4,11 +4,18 @@
 unset OUTDIR
 setenv OUTDIR Output_full_data_3_test2
 
-# Set the hipo files to run over
-unset HIPO_FILES
-# setenv HIPO_FILES /cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/* # All D2 @ 6GeV data
-setenv HIPO_FILES "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo" # Only run 015045 of D2 @ 6GeV data
-# setenv HIPO_FILES /cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015443/*.hipo # Only run 015443 of D2 @ 6GeV data
-# setenv HIPO_FILES /cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo # Only one file of run 015045 of D2 @ 6GeV data
+# # All D2 @ 6GeV data ---------------------------------------------------------------
+# clas12root -l -q 'D_getfeatures_Phase5.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "${OUTDIR}/Andrew_plots.pdf")'
+# # ----------------------------------------------------------------------------------
 
-clas12root -l -q 'D_getfeatures_Phase5.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", ${HIPO_FILES}, "${OUTDIR}/Andrew_plots.pdf")'
+# Only run 015045 of D2 @ 6GeV data ------------------------------------------------
+clas12root -l -q 'D_getfeatures_Phase5.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "${OUTDIR}/Andrew_plots.pdf")'
+# ----------------------------------------------------------------------------------
+
+# # Only run 015443 of D2 @ 6GeV data ------------------------------------------------
+# clas12root -l -q 'D_getfeatures_Phase5.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015443/*.hipo", "${OUTDIR}/Andrew_plots.pdf")'
+# # ----------------------------------------------------------------------------------
+
+# # Only one file of run 015045 of D2 @ 6GeV data ------------------------------------
+# clas12root -l -q 'D_getfeatures_Phase5.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "${OUTDIR}/Andrew_plots.pdf")'
+# # ----------------------------------------------------------------------------------
