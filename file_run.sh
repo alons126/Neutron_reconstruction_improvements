bd45b9316379d0270a2735983c5679556f5bb434
alons126 <alonsportes@mail.tau.ac.il>
2025-01-10 14:32:48 +0200

    Output_data_P8_run2_1_WithPhiNeutNearCuts_test

--- file_run.sh
+++ file_run.sh
@@ -1,8 +1,9 @@
 #!/bin/tcsh
 
 # Set the output directory (/w/hallb-scshelf2102/clas12/asportes/Neutron_reconstruction_improvements)
+setenv OUTDIR Output_data_P8_run2_1_WithPhiNeutNearCuts_test
 # setenv OUTDIR Output_data_P8_run2_full_WithPhiNeutNearCuts
-setenv OUTDIR Output_data_P8_run2_full_NoPhiNeutNearCuts
+# setenv OUTDIR Output_data_P8_run2_full_NoPhiNeutNearCuts
 
 # # Only run 015045 of D2 @ 6GeV data (1 file) ---------------------------------------
 # clas12root -l -q 'ManualVeto_Phase8.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "${OUTDIR}/Andrew_plots.pdf")'
