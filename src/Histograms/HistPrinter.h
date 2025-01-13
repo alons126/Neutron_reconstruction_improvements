114c62100108c8de3633d90fd9f25b6b9b937cda
alons126 <alonsportes@mail.tau.ac.il>
2025-01-13 14:54:35 +0200

    Output_data_P8_run3_1_test

--- src/Histograms/HistPrinter.h
+++ src/Histograms/HistPrinter.h
@@ -410,8 +410,6 @@ void SectionPlotter(int n_col, int n_row, TCanvas *myCanvas, TCanvas *myText, TC
                         int Num_of_goodN_Step5_epCDn = GetHistogramEntries(HistoList, "beta_n_VS_Edep_CND_goodN_Step5_epCDn");
                         int Num_of_badN_Step5_epCDn = GetHistogramEntries(HistoList, "beta_n_VS_Edep_CND_badN_Step5_epCDn");
 
-                        titles.DrawLatex(0.05, 0.9, "Step by step statistics - CD proton");
-
                         // Draw a frame without axis numbers and ticks
                         TH2F *frame_epCDn = new TH2F("frame_epCDn", "", 6, 0, 6, 7, 0, 7);
                         frame_epCDn->SetStats(0);                  // Disable statistics box
@@ -419,7 +417,9 @@ void SectionPlotter(int n_col, int n_row, TCanvas *myCanvas, TCanvas *myText, TC
                         frame_epCDn->GetXaxis()->SetTickLength(0); // Remove x-axis ticks
                         frame_epCDn->GetYaxis()->SetLabelSize(0);  // Remove y-axis labels
                         frame_epCDn->GetYaxis()->SetTickLength(0); // Remove y-axis ticks
-                        frame_epCDn->Draw("same");
+                        frame_epCDn->Draw();
+
+                        titles.DrawLatexNDC(0.05, 0.9, "Step by step statistics - CD proton");
 
                         // Create an instance of TLatex
                         TLatex latex_epCDn;
@@ -485,8 +485,6 @@ void SectionPlotter(int n_col, int n_row, TCanvas *myCanvas, TCanvas *myText, TC
                         int Num_of_goodN_Step5_epFDn = GetHistogramEntries(HistoList, "beta_n_VS_Edep_CND_goodN_Step5_epFDn");
                         int Num_of_badN_Step5_epFDn = GetHistogramEntries(HistoList, "beta_n_VS_Edep_CND_badN_Step5_epFDn");
 
-                        titles.DrawLatex(0.05, 0.9, "Step by step statistics - FD proton");
-
                         // Draw a frame without axis numbers and ticks
                         TH2F *frame_epFDn = new TH2F("frame_epFDn", "", 6, 0, 6, 7, 0, 7);
                         frame_epFDn->SetStats(0);                  // Disable statistics box
@@ -494,7 +492,9 @@ void SectionPlotter(int n_col, int n_row, TCanvas *myCanvas, TCanvas *myText, TC
                         frame_epFDn->GetXaxis()->SetTickLength(0); // Remove x-axis ticks
                         frame_epFDn->GetYaxis()->SetLabelSize(0);  // Remove y-axis labels
                         frame_epFDn->GetYaxis()->SetTickLength(0); // Remove y-axis ticks
-                        frame_epFDn->Draw("same");
+                        frame_epFDn->Draw();
+
+                        titles.DrawLatexNDC(0.05, 0.9, "Step by step statistics - FD proton");
 
                         // Create an instance of TLatex
                         TLatex latex_epFDn;
