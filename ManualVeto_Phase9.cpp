459470c987193e19d60b29aee2b894e5ff9a5689
alons126 <alonsportes@mail.tau.ac.il>
2025-01-20 13:28:40 +0200

    Output_data_P9_run5_57_reformaTest

--- ManualVeto_Phase9.cpp
+++ ManualVeto_Phase9.cpp
@@ -20477,9 +20477,6 @@ int ManualVeto_Phase9( //
         //     h_n_multiplicity_badN_epFDn_Step5->Fill(counter_n_multiplicity_badN_epFDn_Step5, weight);
         // }
 
-
-    cout << "\n\n\nTEST TEST TEST\n\n\n";
-
         histograms.UpdateMultiplicityHistograms(pInCD, pInFD, counter_n_multiplicity_allN_epCDn_Step0,
                                                 counter_n_multiplicity_goodN_epCDn_Step0, counter_n_multiplicity_badN_epCDn_Step0,
                                                 counter_n_multiplicity_allN_epCDn_Step1, counter_n_multiplicity_goodN_epCDn_Step1,
