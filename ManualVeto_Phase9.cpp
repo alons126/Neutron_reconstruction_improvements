986f9f4848db8749e535531462a62425c14286a0
alons126 <alonsportes@mail.tau.ac.il>
2025-01-20 21:13:37 +0200

    Output_data_P9_run5_full_reformaTest_ProxNoNeutNoPos_NoCTOF

--- ManualVeto_Phase9.cpp
+++ ManualVeto_Phase9.cpp
@@ -722,7 +722,7 @@ int ManualVeto_Phase9( //
                                             weight);
 
             // Cutting out neutrons with nearby hits from charged particle tracks
-            if (Nearby_clusters_from_cPart_tracks) { continue; }
+            // if (Nearby_clusters_from_cPart_tracks) { continue; }
 
             // if (Nearby_clusters_from_nPart_tracks) { continue; }
 
