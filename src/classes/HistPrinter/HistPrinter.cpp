b65e909112c32ea8b6d670abb3ab0ebd705b88c6
alons126 <alonsportes@mail.tau.ac.il>
2025-01-22 18:37:16 +0200

    end of day backup

--- src/classes/HistPrinter/HistPrinter.cpp
+++ src/classes/HistPrinter/HistPrinter.cpp
@@ -7,7 +7,7 @@
 // PrintPage function
 // ======================================================================================================================================================================
 
-void HistPrinter::PrintPage(const std::string &PageTitle, TCanvas *myText, char fileName[100], TLatex &titles, TLatex &text,
+void HistPrinter::PrintPage(const std::string &PageTitle, TCanvas *myText, char fileName[100], TLatex titles, TLatex text,
                             const std::string &Constraint1, const std::string &Constraint2) {
     if (PageTitle == "Manual Veto Plots") {
         if (Constraint2 == "") {
@@ -172,8 +172,8 @@ void HistPrinter::PrintPage(const std::string &PageTitle, TCanvas *myText, char 
 // SummaryTablePlotter function
 // ======================================================================================================================================================================
 
-void HistPrinter::GenerateSummaryTable(int n_col, int n_row, TCanvas *myCanvas, TCanvas *myText, TCanvas *myTable, vector<TH1 *> &HistoList,
-                                       TLatex &titles, TLatex &text, char fileName[100], string PDFFile, string Constraint1, string Constraint2,
+void HistPrinter::GenerateSummaryTable(int n_col, int n_row, TCanvas *myCanvas, TCanvas *myText, TCanvas *myTable, vector<TH1 *> HistoList,
+                                       TLatex titles, TLatex text, char fileName[100], string PDFFile, string Constraint1, string Constraint2,
                                        bool LogScale2D) {
     if (First_table_epCDn_generation && (Constraint1 == "" || Constraint1 == "CD")) {
         myTable->SetTopMargin(0.15);
@@ -286,8 +286,8 @@ void HistPrinter::GenerateSummaryTable(int n_col, int n_row, TCanvas *myCanvas, 
 // SummaryTablePlotter function
 // ======================================================================================================================================================================
 
-void HistPrinter::SummaryTablePlotter(int n_col, int n_row, TCanvas *myCanvas, TCanvas *myText, TCanvas *myTable, vector<TH1 *> &HistoList,
-                                      TLatex &titles, TLatex &text, char fileName[100], string PDFFile, string Constraint1, string Constraint2,
+void HistPrinter::SummaryTablePlotter(int n_col, int n_row, TCanvas *myCanvas, TCanvas *myText, TCanvas *myTable, vector<TH1 *> HistoList,
+                                      TLatex titles, TLatex text, char fileName[100], string PDFFile, string Constraint1, string Constraint2,
                                       bool LogScale2D) {
     if (Constraint1 == "" || Constraint1 == "CD") {
         myTable->SetTopMargin(0.15);
@@ -544,7 +544,7 @@ void HistPrinter::SummaryTablePlotter(int n_col, int n_row, TCanvas *myCanvas, T
 // GetHistogramEntries function
 // ======================================================================================================================================================================
 
-double HistPrinter::GetHistogramEntries(const std::vector<TH1 *> &HistoList, const std::string &histName) {
+double HistPrinter::GetHistogramEntries(const std::vector<TH1 *> HistoList, const std::string &histName) {
     for (const auto &hist: HistoList) {
         if (hist && hist->GetName() == histName) {
             // Check if the histogram exists and the name matches
@@ -609,9 +609,9 @@ std::string HistPrinter::replaceSubstring(const std::string &input, const std::s
 // SectionPlotter function
 // ======================================================================================================================================================================
 
-void HistPrinter::SectionPlotter(int n_col, int n_row, TCanvas *myCanvas, TCanvas *myText, TCanvas *myTable, vector<TH1 *> &HistoList,
+void HistPrinter::SectionPlotter(int n_col, int n_row, TCanvas *myCanvas, TCanvas *myText, TCanvas *myTable, vector<TH1 *> HistoList,
                                  string PDFFile, string Constraint1, string Constraint2, bool LogScale2D) {
-    TLatex &titles, text;
+    TLatex titles, text;
     titles.SetTextSize(0.065);
     text.SetTextSize(0.04);
 
@@ -890,7 +890,7 @@ void HistPrinter::SectionPlotter(int n_col, int n_row, TCanvas *myCanvas, TCanva
 // PlotHistograms function
 // ======================================================================================================================================================================
 
-void HistPrinter::PlotHistograms(const vector<TH1 *> &HistoList, const string &PDFFile, bool LogScale2D) {
+void HistPrinter::PlotHistograms(const vector<TH1 *> HistoList, const string &PDFFile, bool LogScale2D) {
     /////////////////////////////////////////////////////
     // Now create the output PDFs
     /////////////////////////////////////////////////////
