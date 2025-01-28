29adff41b6ea78309c7d4350ba609f70b40a9cf7
alons126 <alonsportes@mail.tau.ac.il>
2025-01-28 19:58:20 +0200

    Output_data_P9_run9_full_OnlyLayerMultCut

--- file_run.sh
+++ file_run.sh
@@ -1,7 +1,8 @@
 #!/bin/tcsh
 
 # Set the output directory (/w/hallb-scshelf2102/clas12/asportes/Neutron_reconstruction_improvements/)
-setenv OUTDIR Output_data_P9_run9_full_OnlySizeCut
+setenv OUTDIR Output_data_P9_run9_full_OnlyLayerMultCut
+# setenv OUTDIR Output_data_P9_run9_full_OnlySizeCut
 # setenv OUTDIR Output_data_P9_run9_full_OnlyNeutProxCut
 # setenv OUTDIR Output_data_P9_run9_full_OnlyPosProxCut
 # setenv OUTDIR Output_data_P9_run9_full_NoNeutProxCut
