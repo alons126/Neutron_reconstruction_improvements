d9873e4e09d5259dec2b0dadd0a23bdf2a07d4db
alons126 <alonsportes@mail.tau.ac.il>
2025-01-28 22:24:14 +0200

    Output_data_P9_run9_full_OnlyPosProxCut_OnlydToF

--- ManualVeto_Phase9.cpp
+++ ManualVeto_Phase9.cpp
@@ -653,7 +653,7 @@ int ManualVeto_Phase9(                            //
 
                     if (                               // Set the cut on neutrons with nearby clusters from charged particle tracks:
                                                        // Bad_sdiff_of1_CutCondition ||                                  // Minimal sdiff is 2
-                        Bad_sdiff_of1_CutCondition 
+                        // Bad_sdiff_of1_CutCondition 
                         // ||  // Minimal sdiff is 3
                         // // isPosNear_PhiCut(sdiff, ldiff, P_n_3v.Phi() * 180. / M_PI) ||  // Phi_n cut
                         isPosNear_dToF(sdiff, ldiff, dToF)  // ToF difference cut
