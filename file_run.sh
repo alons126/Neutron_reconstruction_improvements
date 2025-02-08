f479fe3947bba0cb3bd175a1906aafd0025d225c
alons126 <alonsportes@mail.tau.ac.il>
2025-02-08 16:32:12 +0200

    Output_data_P9_run9_57_REDO_new

--- file_run.sh
+++ file_run.sh
@@ -1,17 +1,18 @@
 #!/bin/tcsh
 
 # Set the output directory (/w/hallb-scshelf2102/clas12/asportes/Neutron_reconstruction_improvements/)
-setenv OUTDIR Output_data_P9_run9_1_REDO_new
+setenv OUTDIR Output_data_P9_run9_57_REDO_new
+# setenv OUTDIR Output_data_P9_run9_1_REDO_new
 # setenv OUTDIR Output_data_P9_run9_57_REDO
 # setenv OUTDIR Output_data_P9_run9_1_REDO
 
- # Only run 015045 of D2 @ 6GeV data (1 file) ---------------------------------------
- clas12root -l -q 'ManualVeto_Phase9.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "${OUTDIR}/ManualVeto_plots.pdf")'
- # ----------------------------------------------------------------------------------
+#  # Only run 015045 of D2 @ 6GeV data (1 file) ---------------------------------------
+#  clas12root -l -q 'ManualVeto_Phase9.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "${OUTDIR}/ManualVeto_plots.pdf")'
+#  # ----------------------------------------------------------------------------------
 
-# # Only run 015449 of D2 @ 6GeV data (57 files) ------------------------------------
-# clas12root -l -q 'ManualVeto_Phase9.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015449/*.hipo", "${OUTDIR}/ManualVeto_plots.pdf")'
-# # ----------------------------------------------------------------------------------
+# Only run 015449 of D2 @ 6GeV data (57 files) ------------------------------------
+clas12root -l -q 'ManualVeto_Phase9.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015449/*.hipo", "${OUTDIR}/ManualVeto_plots.pdf")'
+# ----------------------------------------------------------------------------------
 
 # # Only run 015045 of D2 @ 6GeV data (131 files) ------------------------------------
 # clas12root -l -q 'ManualVeto_Phase9.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/*.hipo", "${OUTDIR}/ManualVeto_plots.pdf")'
