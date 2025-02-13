4a35ac005b0bbcd0e2b150749693cbb7d76c8628
alons126 <alonsportes@mail.tau.ac.il>
2025-02-13 19:40:10 +0200

    Output_data_P9_run9_full_newBetaAndToF_NoCTOF

--- file_run.sh
+++ file_run.sh
@@ -1,8 +1,8 @@
 #!/bin/tcsh
 
 # Set the output directory (/w/hallb-scshelf2102/clas12/asportes/Neutron_reconstruction_improvements/)
-# setenv OUTDIR Output_data_P9_run9_full_newBetaAndToF_NoCTOF
-setenv OUTDIR Output_data_P9_run9_full_newBetaAndToF
+setenv OUTDIR Output_data_P9_run9_full_newBetaAndToF_NoCTOF
+# setenv OUTDIR Output_data_P9_run9_full_newBetaAndToF
 
 #  # Only run 015045 of D2 @ 6GeV data (1 file) ---------------------------------------
 #  clas12root -l -q 'ManualVeto_Phase9.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015045/rec_clas_015045.evio.00650-00654.hipo", "${OUTDIR}/ManualVeto_plots.pdf")'
