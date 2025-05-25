08ef018b58006f4c40c9c99009e7885702d2903d
alons126 <alonsportes@mail.tau.ac.il>
2025-05-25 12:54:45 +0300

    test 4

--- file_run.sh
+++ file_run.sh
@@ -4,25 +4,25 @@
 setenv OUTDIR Output_data_P11_3
 
  # Only run 015050 of D2 @ 6GeV data (1 file) ---------------------------------------
- clas12root -l -q 'ManualNeutronVeto.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015050/rec_clas_015050.evio.00210-00214.hipo", "${OUTDIR}/ManualVeto_plots.pdf")'
+ clas12root -l -q 'ManualNeutronVeto.cpp("${OUTDIR}", "Erin_plots.pdf", 5.98636, true, "Erin_plots.root", "Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015050/rec_clas_015050.evio.00210-00214.hipo", "ManualVeto_plots.pdf")'
  # ----------------------------------------------------------------------------------
 
 # # Only run 015449 of D2 @ 6GeV data (57 files) ------------------------------------
-# clas12root -l -q 'ManualNeutronVeto.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015449/*.hipo", "${OUTDIR}/ManualVeto_plots.pdf")'
+# clas12root -l -q 'ManualNeutronVeto.cpp("${OUTDIR}", "Erin_plots.pdf", 5.98636, true, "Erin_plots.root", "Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015449/*.hipo", "ManualVeto_plots.pdf")'
 # # ----------------------------------------------------------------------------------
 
 # # Only run 015050 of D2 @ 6GeV data (131 files) ------------------------------------
-# clas12root -l -q 'ManualNeutronVeto.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015050/*.hipo", "${OUTDIR}/ManualVeto_plots.pdf")'
+# clas12root -l -q 'ManualNeutronVeto.cpp("${OUTDIR}", "Erin_plots.pdf", 5.98636, true, "Erin_plots.root", "Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015050/*.hipo", "ManualVeto_plots.pdf")'
 # # ----------------------------------------------------------------------------------
 
 # # Only run 015443 of D2 @ 6GeV data (370 files) ------------------------------------
-# clas12root -l -q 'ManualNeutronVeto.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015443/*.hipo", "${OUTDIR}/ManualVeto_plots.pdf")'
+# clas12root -l -q 'ManualNeutronVeto.cpp("${OUTDIR}", "Erin_plots.pdf", 5.98636, true, "Erin_plots.root", "Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/015443/*.hipo", "ManualVeto_plots.pdf")'
 # # ----------------------------------------------------------------------------------
 
 # # Only run 015443 of D2 @ 6GeV data (1292 files) ------------------------------------
-# clas12root -l -q 'ManualNeutronVeto.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/333", "${OUTDIR}/ManualVeto_plots.pdf")'
+# clas12root -l -q 'ManualNeutronVeto.cpp("${OUTDIR}", "Erin_plots.pdf", 5.98636, true, "Erin_plots.root", "Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/333", "ManualVeto_plots.pdf")'
 # # ----------------------------------------------------------------------------------
 
 # # All D2 @ 6GeV data ---------------------------------------------------------------
-# clas12root -l -q 'ManualNeutronVeto.cpp("${OUTDIR}", "${OUTDIR}/Erin_plots.pdf", 5.98636, true, "${OUTDIR}/Erin_plots.root", "${OUTDIR}/Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "${OUTDIR}/ManualVeto_plots.pdf")'
+# clas12root -l -q 'ManualNeutronVeto.cpp("${OUTDIR}", "Erin_plots.pdf", 5.98636, true, "Erin_plots.root", "Erin_plots.txt", "/cache/clas12/rg-m/production/pass1/6gev/D/dst/recon/*", "ManualVeto_plots.pdf")'
 # # ----------------------------------------------------------------------------------
